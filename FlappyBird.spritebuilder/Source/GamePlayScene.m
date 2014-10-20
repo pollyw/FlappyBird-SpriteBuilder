adbc648ac63eb5fc716f042192ded914497db313
Polly Wu <pollywu@pollys-air.gateway.sonic.net>
2014-10-20 13:26:59 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,11 @@
 - (void)initialize
 {
     // your code here
+    //load new character from ccb file made by spritebuilder
+    character = (Character*)[CCBReader load:@"Character"];
+    
+    //add character as a child of physicsnode so physics will be applied ot it
+    [physicsNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
@@ -15,5 +20,11 @@
 }
 
 // put new methods here
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    //this will be called everytime the player touches the screen
+    //whenever screen touch, method flap will apply to character
+    [character flap];
+
+}
 
 @end
